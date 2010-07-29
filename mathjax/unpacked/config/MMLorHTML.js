16ef7aa7eabca674710e25aa8a1b754be378137f
Davide Cervone <dpvc@union.edu>
2010-07-29 20:11:31 +0000

    Second step of reorganizing diretory structure
    git-svn-id: https://mathjax.svn.sourceforge.net/svnroot/mathjax/trunk@530 b8fd5906-0fad-46e2-a0d3-10d94ff285d1

---
+++ mathjax/unpacked/config/MMLorHTML.js
@@ -0,0 +1,88 @@
+/*************************************************************
+ *
+ *  MathJax/config/MMLorHTML.js
+ *  
+ *  Chooses between the NativeMML and HTML-CSS output jax depending
+ *  on the capabilities of the browser and configuration settings
+ *  of the page.
+ *  
+ *  This file should be added to the config array when configuring
+ *  MathJax.  Note that if you include this, you should NOT include
+ *  an output jax in the jax array (it will be added for you by
+ *  this file).
+ *  
+ *  You can specify the preferred output jax on a global or
+ *  browser-by-browser basis.  To specify it globally, use
+ *  
+ *      MathJax.Hub.Config({
+ *        MMLorHTML: {prefer: "MML"} // or "HTML"
+ *      });
+ *  
+ *  To specify on a browser-by-borwser basis, use
+ *  
+ *      MathJax.Hub.Config({
+ *        MMLorHTML: {prefer: {
+ *          MSIE:    "MML",
+ *          Firefox: "MML",
+ *          Opera:   "HTML",
+ *          other:   "HTML"
+ *        }}
+ *      });
+ *
+ *  ---------------------------------------------------------------------
+ *  
+ *  Copyright (c) 2010 Design Science, Inc.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+(function (HUB) {
+  var CONFIG = MathJax.Hub.Insert({
+    prefer: {MSIE:"MML", Firefox:"MML", Opera:"HTML", other:"HTML"}
+  },(MathJax.Hub.config.MMLorHTML||{}));
+
+  var MINBROWSERVERSION = {
+    Firefox: 3.0,
+    Opera: 9.52,
+    MSIE: 6.0,
+    Chrome: 0.3,
+    Safari: 2.0,
+    Konqueror: 4.0
+  };
+
+  var canUseHTML = (HUB.Browser.version === "0.0" ||
+                    HUB.Browser.versionAtLeast(MINBROWSERVERSION[HUB.Browser]||0.0));
+
+  var MathPlayer; try {new ActiveXObject("MathPlayer.Factory.1"); MathPlayer = true} catch(err) {MathPlayer = false};
+
+  var canUseMML = (HUB.Browser.isFirefox && HUB.Browser.versionAtLeast("1.5")) ||
+                  (HUB.Browser.isMSIE && MathPlayer) ||
+                  (HUB.Browser.isOpera && HUB.Browser.versionAtLeast("9.52"));
+
+  var prefer = (CONFIG.prefer && typeof(CONFIG.prefer) === "object" ? 
+                CONFIG.prefer[MathJax.Hub.Browser]||CONFIG.prefer.other||"HTML" :
+                CONFIG.prefer);   
+
+  if (canUseHTML || canUseMML) {
+    if (canUseMML && (prefer === "MML" || !canUseHTML))
+      {HUB.config.jax.unshift("output/NativeMML")} else {HUB.config.jax.unshift("output/HTML-CSS")}
+  } else {
+    HUB.PreProcess.disabled = true;
+    HUB.prepareScripts.disabled = true;
+    MathJax.Message.Set("Your browser does not support MathJax",null,4000);
+    HUB.Startup.signal.Post("MathJax not supported");
+  }
+
+})(MathJax.Hub);
+
+MathJax.Ajax.loadComplete("[MathJax]/config/MMLorHTML.js");
---
+++ mathjax/unpacked/extensions/mml2jax.js
@@ -0,0 +1,127 @@
+/*************************************************************
+ *
+ *  MathJax/extensions/mml2jax.js
+ *  
+ *  Implements the MathML to Jax preprocessor that locates <math> nodes
+ *  within the text of a document and replaces them with SCRIPT tags
+ *  for processing by MathJax.
+ *
+ *  ---------------------------------------------------------------------
+ *  
+ *  Copyright (c) 2010 Design Science, Inc.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+MathJax.Extension.mml2jax = {
+  config: {
+    element: null,          // The ID of the element to be processed
+                            //   (defaults to full document)
+
+    preview: "alttext"      // Use the <math> element's alttext as the 
+                            //   preview.  Set to "none" for no preview,
+                            //   or set to an array specifying an HTML snippet
+                            //   to use a fixed preview for all math
+
+  },
+  MMLnamespace: "http://www.w3.org/1998/Math/MathML",
+  
+  PreProcess: function (element) {
+    if (!this.configured) {
+      MathJax.Hub.Insert(this.config,(MathJax.Hub.config.mml2jax||{}));
+      if (this.config.Augment) {MathJax.Hub.Insert(this,this.config.Augment)}
+      this.configured = true;
+    }
+    if (typeof(element) === "string") {element = document.getElementById(element)}
+    if (!element) {element = this.config.element || document.body}
+    var math = element.getElementsByTagName("math"), i;
+    if (math.length === 0 && element.getElementsByTagNameNS)
+      {math = element.getElementsByTagNameNS(this.MMLnamespace,"math")}
+    if (this.msieMathTagBug) {
+      for (i = math.length-1; i >= 0; i--) {
+        if (math[i].nodeName === "MATH") {this.msieProcessMath(math[i])}
+                                    else {this.ProcessMath(math[i])}
+      }
+    } else {
+      for (i = math.length-1; i >= 0; i--) {this.ProcessMath(math[i])}
+    }
+  },
+  
+  ProcessMath: function (math) {
+    var parent = math.parentNode;
+    var script = document.createElement("script");
+    script.type = "math/mml";
+    parent.insertBefore(script,math);
+    if (this.msieScriptBug) {
+      var html = math.outerHTML;
+      html = html.replace(/<\?import .*?>/,"").replace(/<\?xml:namespace .*?\/>/,"");
+      html = html.replace(/<(\/?)m:/g,"<$1").replace(/&nbsp;/g,"&#xA0;");
+      script.text = html;
+      parent.removeChild(math);
+    } else {
+      script.appendChild(math);
+    }
+    if (this.config.preview !== "none") {this.createPreview(math,script)}
+  },
+  
+  msieProcessMath: function (math) {
+    var parent = math.parentNode;
+    var script = document.createElement("script");
+    script.type = "math/mml";
+    parent.insertBefore(script,math);
+    var mml = "";
+    while (math && math.nodeName !== "/MATH") {
+      if (math.nodeName === "#text" || math.nodeName === "#comment")
+        {mml += math.nodeValue.replace(/&/g,"&#x26;").replace(/</g,"&lt;").replace(/>/g,"&gt;")}
+        else {mml += this.toLowerCase(math.outerHTML)}
+      var node = math;
+      math = math.nextSibling;
+      node.parentNode.removeChild(node);
+    }
+    if (math && math.nodeName === "/MATH") {math.parentNode.removeChild(math)}
+    script.text = mml + "</math>";
+    if (this.config.preview !== "none") {this.createPreview(math,script)}
+  },
+  toLowerCase: function (string) {
+    var parts = string.split(/"/);
+    for (var i = 0, m = parts.length; i < m; i += 2) {parts[i] = parts[i].toLowerCase()}
+    return parts.join('"');
+  },
+  
+  createPreview: function (math,script) {
+    var preview;
+    if (this.config.preview === "alttext") {
+      var text = math.getAttribute("alttext");
+      if (text != null) {preview = [this.filterText(text)]}
+    } else if (this.config.preview instanceof Array) {preview = this.config.preview}
+    if (preview) {
+      preview = MathJax.HTML.Element("span",{className:MathJax.Hub.config.preRemoveClass},preview);
+      script.parentNode.insertBefore(preview,script);
+    }
+  },
+  
+  filterText: function (text) {return text}
+
+};
+
+MathJax.Hub.Browser.Select({
+  MSIE: function (browser) {
+    MathJax.Hub.Insert(MathJax.Extension.mml2jax,{
+      msieScriptBug: true,
+      msieMathTagBug: true
+    })
+  }
+});
+  
+MathJax.Hub.Register.PreProcessor(["PreProcess",MathJax.Extension.mml2jax]);
+MathJax.Ajax.loadComplete("[MathJax]/extensions/mml2jax.js");
