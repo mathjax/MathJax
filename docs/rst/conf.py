c2d131c42bd7108c4885589f4f7aba45ba53a17e
Casey W. Stark <casey@thestarkeffect.com>
2011-01-21 20:50:18 -0800

    Fixed up the sphinx theme to make it look like the current mathjax.org theme. Should change some links and add the logo to _static to work well offline

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -22,9 +22,7 @@ sys.path.append(os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath']
-#extensions = ['sphinx.ext.jsmath']
-#jsmath_path = 'mathjax/MathJax.js'
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MathJax'
-copyright = u'2010 Design Science'
+copyright = u'2011 Design Science'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
