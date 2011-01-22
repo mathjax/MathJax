1ecd7fbd49daf77b8ca4ceeee1060ffda9593cd9
Davide P. Cervone <dpvc@union.edu>
2011-01-21 20:18:16 -0500

    Fix problem with \over used inside array entries (resolves issue#54)

--- unpacked/jax/input/TeX/config.js
+++ unpacked/jax/input/TeX/config.js
@@ -24,7 +24,7 @@
 
 MathJax.InputJax.TeX = MathJax.InputJax({
   id: "TeX",
-  version: "1.0.4",
+  version: "1.0.5",
   directory: MathJax.InputJax.directory + "/TeX",
   extensionDir: MathJax.InputJax.extensionDir + "/TeX",
   
