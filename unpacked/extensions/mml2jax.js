2a99d528aac32c54b90dfdac11e44b08b7773cbc
Davide P. Cervone <dpvc@union.edu>
2011-02-02 10:59:34 -0500

    Comments don't need to have <, >, and & replaced, so do text separately from comments

--- unpacked/extensions/mml2jax.js
+++ unpacked/extensions/mml2jax.js
@@ -24,7 +24,7 @@
  */
 
 MathJax.Extension.mml2jax = {
-  varsion: "1.0.4",
+  varsion: "1.0.5",
   config: {
     element: null,          // The ID of the element to be processed
                             //   (defaults to full document)
@@ -116,9 +116,10 @@ MathJax.Extension.mml2jax = {
   },
   msieNodeHTML: function (node) {
     var html, i, m;
-    if (node.nodeName === "#text" || node.nodeName === "#comment") {
+    if (node.nodeName === "#text") {
       html = node.nodeValue.replace(/&/g,"&#x26;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
-      if (node.nodeName === "#comment") {html = "<!--"+html+"-->"}
+    } else if (node.nodeName === "#comment") {
+      html = "<!--" + node.nodeValue + "-->"
     }
     else if (this.msieAttributeBug) {
       // In IE, outerHTML doesn't properly quote attributes, so quote them by hand
