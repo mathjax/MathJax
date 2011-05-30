8588f0241303ea2b3c3071bb5ecebbed4003a450
Davide P. Cervone <dpvc@union.edu>
2011-05-30 14:05:35 -0400

    Remove extra comma from RANGES (causes IE crash), bump version numbers, and rename IE Greek ranges to work properly with new greek range in 1.1a

--- unpacked/jax/output/HTML-CSS/fonts/TeX/fontdata.js
+++ unpacked/jax/output/HTML-CSS/fonts/TeX/fontdata.js
@@ -7,7 +7,7 @@
  *
  *  ---------------------------------------------------------------------
  *  
- *  Copyright (c) 2009 Design Science, Inc.
+ *  Copyright (c) 2009-2011 Design Science, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@
  */
 
 (function (HTMLCSS,MML,AJAX) {
-  var VERSION = "1.0.1";
+  var VERSION = "1.1.1";
   
   var MAIN   = "MathJax_Main",
       BOLD   = "MathJax_Main-bold",
@@ -104,7 +104,7 @@
       RANGES: [
         {name: "alpha", low: 0x61, high: 0x7A, offset: "A", add: 32},
         {name: "number", low: 0x30, high: 0x39, offset: "N"},
-        {name: "greek", low: 0x03B1, high: 0x03F6, offset: "G"},
+        {name: "greek", low: 0x03B1, high: 0x03F6, offset: "G"}
       ],
       
       RULECHAR: 0x2212,
@@ -1411,7 +1411,7 @@
     MathJax.Hub.Browser.Select({
       MSIE: function (browser) {
         
-        if (!HTMLCSS.imgFonts && HTMLCSS.config.availableFonts && HTMLCSS.config.availableFonts.length) {
+        if (HTMLCSS.config.availableFonts && HTMLCSS.config.availableFonts.length) {
           
           HTMLCSS.FONTDATA.REMAP[0x2C9] = 0xAF; // macron
           HTMLCSS.FONTDATA.REMAP[0x2CA] = 0xB4; // acute
@@ -1421,18 +1421,18 @@
           var testString = HTMLCSS.msieCheckGreek =
             String.fromCharCode(0x393)+" "+String.fromCharCode(0x3A5)+" "+String.fromCharCode(0x39B);
 
-          HTMLCSS.FONTDATA.RANGES.push({name: "greek", low: 0x03B1, high: 0x03C9, offset: "G", add: 32});
-          HTMLCSS.FONTDATA.RANGES.push({name: "Greek", low: 0x0391, high: 0x03F6, offset: "G"});
+          HTMLCSS.FONTDATA.RANGES.push({name: "IEgreek", low: 0x03B1, high: 0x03C9, offset: "IEG", add: 32});
+          HTMLCSS.FONTDATA.RANGES.push({name: "IEGreek", low: 0x0391, high: 0x03F6, offset: "IEG"});
           
           if (HTMLCSS.Font.testFont({family:"MathJax_Greek", testString: testString})) {
             HTMLCSS.Augment({
               FONTDATA: {
                 VARIANT: {
-                  normal:             {offsetG: 0x391, variantG: "-Greek"},
-                  "fraktur":          {offsetG: 0x391, variantG: "-Greek"},
-                  "script":           {offsetG: 0x391, variantG: "-Greek"},
-                  "-tex-caligraphic": {offsetG: 0x391, variantG: "-Greek"},
-                  "-tex-oldstyle":    {offsetG: 0x391, variantG: "-Greek"},
+                  normal:             {offsetIEG: 0x391, variantIEG: "-Greek"},
+                  "fraktur":          {offsetIEG: 0x391, variantIEG: "-Greek"},
+                  "script":           {offsetIEG: 0x391, variantIEG: "-Greek"},
+                  "-tex-caligraphic": {offsetIEG: 0x391, variantIEG: "-Greek"},
+                  "-tex-oldstyle":    {offsetIEG: 0x391, variantIEG: "-Greek"},
                   "-Greek":           {fonts:["MathJax_Greek"]}
                 }
               }
@@ -1463,9 +1463,9 @@
             HTMLCSS.Augment({
               FONTDATA: {
                 VARIANT: {
-                  bold:               {offsetG: 0x391, variantG: "-Greek-Bold"},
-                  "bold-fraktur":     {offsetG: 0x391, variantG: "-Greek-Bold"},
-                  "bold-script":      {offsetG: 0x391, variantG: "-Greek-Bold"},
+                  bold:               {offsetIEG: 0x391, variantIEG: "-Greek-Bold"},
+                  "bold-fraktur":     {offsetIEG: 0x391, variantIEG: "-Greek-Bold"},
+                  "bold-script":      {offsetIEG: 0x391, variantIEG: "-Greek-Bold"},
                   "-Greek-Bold":      {fonts:["MathJax_Greek-bold"]}
                 }
               }
@@ -1497,7 +1497,7 @@
             HTMLCSS.Augment({
               FONTDATA: {
                 VARIANT: {
-                  italic:  {offsetG: 0x391, variantG: "-Greek-Italic"},
+                  italic:  {offsetIEG: 0x391, variantIEG: "-Greek-Italic"},
                   "-Greek-Italic": {fonts:["MathJax_Greek-italic"]}
                 }
               }
@@ -1591,7 +1591,7 @@
             
           }
         }
-          
+
         if (HTMLCSS.msieIE6) {
           
           var WinIE6 = "MathJax_WinIE6";
