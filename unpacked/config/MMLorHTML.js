0adb4b9d5e45ed1be7696374d9d31dbfdca0a8a5
Davide P. Cervone <dpvc@union.edu>
2012-01-23 12:17:13 -0500

    Make default for Firefox be HTML (since its native MML doesn't support all the needed features), and give explicit defaults for Safari and Chrome.

--- unpacked/config/MMLorHTML.js
+++ unpacked/config/MMLorHTML.js
@@ -46,11 +46,18 @@
  *  limitations under the License.
  */
 
-(function (HUB) {
+(function (HUB,BROWSER) {
   var VERSION = "1.1.1";
   
   var CONFIG = MathJax.Hub.CombineConfig("MMLorHTML",{
-    prefer: {MSIE:"MML", Firefox:"MML", Opera:"HTML", other:"HTML"}
+    prefer: {
+      MSIE:"MML",
+      Firefox:"HTML",
+      Opera:"HTML",
+      Chrome:"HTML",
+      Safari:"HTML",
+      other:"HTML"
+    }
   });
 
   var MINBROWSERVERSION = {
@@ -61,17 +68,14 @@
     Safari: 2.0,
     Konqueror: 4.0
   };
+  
+  var canUseHTML = (BROWSER.version === "0.0" ||
+                    BROWSER.versionAtLeast(MINBROWSERVERSION[BROWSER]||0.0));
 
-  var canUseHTML = (HUB.Browser.version === "0.0" ||
-                    HUB.Browser.versionAtLeast(MINBROWSERVERSION[HUB.Browser]||0.0));
-
-  var MathPlayer = false;
-  try {new ActiveXObject("MathPlayer.Factory.1"); MathPlayer = true} catch(err) {}
-
-  var canUseMML = (HUB.Browser.isFirefox && HUB.Browser.versionAtLeast("1.5")) ||
-                  (HUB.Browser.isMSIE && MathPlayer) ||
-                  (HUB.Browser.isSafari && HUB.Browser.versionAtLeast("5.0")) ||
-                  (HUB.Browser.isOpera && HUB.Browser.versionAtLeast("9.52"));
+  var canUseMML = (BROWSER.isFirefox && BROWSER.versionAtLeast("1.5")) ||
+                  (BROWSER.isMSIE    && BROWSER.hasMathPlayer) ||
+                  (BROWSER.isSafari  && BROWSER.versionAtLeast("5.0")) ||
+                  (BROWSER.isOpera   && BROWSER.versionAtLeast("9.52"));
 
   HUB.Register.StartupHook("End Config",function () {
     var prefer = (CONFIG.prefer && typeof(CONFIG.prefer) === "object" ? 
@@ -96,6 +100,6 @@
     }
   });
 
-})(MathJax.Hub);
+})(MathJax.Hub,MathJax.Hub.Browser);
 
 MathJax.Ajax.loadComplete("[MathJax]/config/MMLorHTML.js");
