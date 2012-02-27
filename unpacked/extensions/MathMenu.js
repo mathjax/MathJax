b8fc1c99f51f7321f59ce1e96ee9aa0837a56c28
Davide P. Cervone <dpvc@union.edu>
2012-02-27 18:19:32 -0500

    Bump the version number on MathMenu.js to account for the previous change

--- unpacked/extensions/MathMenu.js
+++ unpacked/extensions/MathMenu.js
@@ -24,7 +24,7 @@
  */
 
 (function (HUB,HTML,AJAX,CALLBACK,OUTPUT) {
-  var VERSION = "2.0";
+  var VERSION = "2.0.1";
 
   var SIGNAL = MathJax.Callback.Signal("menu")  // signal for menu events
   
