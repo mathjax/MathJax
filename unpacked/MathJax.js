636be43e6a92d8912612993f871bae546d5efd32
Davide P. Cervone <dpvc@union.edu>
2012-03-11 09:50:38 -0400

    Handle order of jax that appear multiple times (properly handle MMLorHTML config)

--- unpacked/MathJax.js
+++ unpacked/MathJax.js
@@ -1862,8 +1862,9 @@ MathJax.Hub.Startup = {
     var config = MathJax.Hub.config, jax = MathJax.Hub.outputJax;
     //  Save the order of the output jax since they are loading asynchronously
     for (var i = 0, m = config.jax.length, k = 0; i < m; i++) {
-      if (config.jax[i].substr(0,7) === "output/") 
-        {jax.order[config.jax[i].substr(7)] = k; k++}
+      var name = config.jax[i].substr(7);
+      if (config.jax[i].substr(0,7) === "output/" && jax.order[name] == null)
+        {jax.order[name] = k; k++}
     }
     var queue = MathJax.Callback.Queue();
     return queue.Push(
