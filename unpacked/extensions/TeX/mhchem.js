7c6b2c01dfa7be06d0adb0371eeb53096e183c02
Davide P. Cervone <dpvc@union.edu>
2012-03-16 09:24:49 -0400

    Expose the CE object (so we can override its methods) and bump the version number

--- unpacked/extensions/TeX/mhchem.js
+++ unpacked/extensions/TeX/mhchem.js
@@ -23,7 +23,7 @@
  */
 
 MathJax.Extension["TeX/mhchem"] = {
-  version: "2.0"
+  version: "2.0.1"
 };
 
 MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
@@ -362,6 +362,7 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
     
   });
   
+  MathJax.Extension["TeX/mhchem"].CE = CE;
   
   /***************************************************************************/
   
