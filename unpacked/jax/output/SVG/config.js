b40ca8cde42029bd812595b1d6cff75a8f264971
Davide P. Cervone <dpvc@union.edu>
2012-08-16 11:35:14 -0400

    Make the changes to font handling to SVG (just like HTML-CSS) so that &#x2032; will be displayed better in AsciiMath output.

--- unpacked/jax/output/SVG/config.js
+++ unpacked/jax/output/SVG/config.js
@@ -24,7 +24,7 @@
 
 MathJax.OutputJax.SVG = MathJax.OutputJax({
   id: "SVG",
-  version: "2.0.2",
+  version: "2.0.3",
   directory: MathJax.OutputJax.directory + "/SVG",
   extensionDir: MathJax.OutputJax.extensionDir + "/SVG",
   autoloadDir: MathJax.OutputJax.directory + "/SVG/autoload",
