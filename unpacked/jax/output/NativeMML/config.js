bb7f76158cf1378b45539b989fe75b43070abf82
Davide P. Cervone <dpvc@union.edu>
2012-08-16 09:12:23 -0400

    Bump version number and recombine after prevous pull from Fred

--- unpacked/jax/output/NativeMML/config.js
+++ unpacked/jax/output/NativeMML/config.js
@@ -24,7 +24,7 @@
 
 MathJax.OutputJax.NativeMML = MathJax.OutputJax({
   id: "NativeMML",
-  version: "2.0.4",
+  version: "2.0.5",
   directory: MathJax.OutputJax.directory + "/NativeMML",
   extensionDir: MathJax.OutputJax.extensionDir + "/NativeMML",
   
