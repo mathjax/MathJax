7b735d6abfa0ddedba3c148d65249fd4aca3d961
Davide P. Cervone <dpvc@union.edu>
2013-05-13 06:04:50 -0400

    Repack previous commit

--- localization/de/HTML-CSS.js
+++ localization/de/HTML-CSS.js
@@ -12,5 +12,5 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  */
 
-MathJax.Localization.addTranslation("de","HTML-CSS",{isLoaded:true,strings:{LoadWebFont:"Lade Webfont %1",CantLoadWebFont:"Kann Webfont %1 nicht laden",FirefoxCantLoadWebFont:"Firefox kann Webfonts nicht von entferntem Computer laden",CantFindFontUsing:"Kein g\u00FCltiger Font fuer %1 verf\u00FCgbar",WebFontsNotAvailable:"Webfonts nicht verf\u00FCgbar -- benutze Bildfont"}});MathJax.Ajax.loadComplete("[MathJax]/localization/de/HTML-CSS.js");
+MathJax.Localization.addTranslation("de","HTML-CSS",{version:"2.2",isLoaded:true,strings:{LoadWebFont:"Lade Webfont %1",CantLoadWebFont:"Kann Webfont %1 nicht laden",FirefoxCantLoadWebFont:"Firefox kann Webfonts nicht von entferntem Computer laden",CantFindFontUsing:"Kein g\u00FCltiger Font fuer %1 verf\u00FCgbar",WebFontsNotAvailable:"Webfonts nicht verf\u00FCgbar -- benutze Bildfont"}});MathJax.Ajax.loadComplete("[MathJax]/localization/de/HTML-CSS.js");
 
