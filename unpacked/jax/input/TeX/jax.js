2b1948f2529af839ba2e76aae32b23ca0c6a7975
Davide P. Cervone <dpvc@union.edu>
2013-05-29 12:32:01 -0400

    Force cases environment to use textstyle rather than displaystyle.  Resolves issue #485.

--- unpacked/jax/input/TeX/jax.js
+++ unpacked/jax/input/TeX/jax.js
@@ -267,6 +267,7 @@
         if (item.isCR)    {this.EndEntry(); this.EndRow(); this.clearEnv(); return false}
         this.EndTable(); this.clearEnv();
         var mml = MML.mtable.apply(MML,this.table).With(this.arraydef);
+debug(show(this.arraydef));
         if (this.frame.length === 4) {
           mml.frame = (this.frame.dashed ? "dashed" : "solid");
         } else if (this.frame.length) {
@@ -992,7 +993,7 @@
         Bmatrix:      ['Array',null,'\\{','\\}','c'],
         vmatrix:      ['Array',null,'\\vert','\\vert','c'],
         Vmatrix:      ['Array',null,'\\Vert','\\Vert','c'],
-        cases:        ['Array',null,'\\{','.','ll',null,".1em"],
+        cases:        ['Array',null,'\\{','.','ll',null,".2em",'T'],
 
         equation:     [null,'Equation'],
         'equation*':  [null,'Equation'],
@@ -1757,6 +1758,7 @@
       if (open)  {array.open  = this.convertDelimiter(open)}
       if (close) {array.close = this.convertDelimiter(close)}
       if (style === "D") {array.arraydef.displaystyle = true}
+         else if (style) {array.arraydef.displaystyle = false}
       if (style === "S") {array.arraydef.scriptlevel = 1} // FIXME: should use mstyle?
       if (raggedHeight)  {array.arraydef.useHeight = false}
       this.Push(begin);
