90749a7afefce9cbd109879288a74e1688546b07
Frédéric Wang <fred.wang@free.fr>
2013-10-21 14:30:16 +0200

    Fix WARNINGs generated by the MathJax packer. #534

--- unpacked/jax/output/SVG/autoload/menclose.js
+++ unpacked/jax/output/SVG/autoload/menclose.js
@@ -116,7 +116,7 @@ MathJax.Hub.Register.StartupHook("SVG Jax Ready",function () {
 
       // perform some reduction e.g. eliminate duplicate notations.
       var nl = MathJax.Hub.SplitList(values.notation), notation = {};
-      for (var i = 0, m = nl.length; i < m; i++) notation[nl[i]] = true;
+      for (i = 0, m = nl.length; i < m; i++) notation[nl[i]] = true;
       if (notation[MML.NOTATION.UPDIAGONALARROW]) notation[MML.NOTATION.UPDIAGONALSTRIKE] = false;
       
       for (var n in notation) {
