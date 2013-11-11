ef36d4b2a5ce873ddcc8d3bb9b57fb095723a869
Frédéric Wang <fred.wang@free.fr>
2013-11-11 14:30:46 +0100

    Repack and combine. #534

--- localization/de/HelpDialog.js
+++ localization/de/HelpDialog.js
@@ -1,16 +1,19 @@
 /*
  *  /MathJax/localization/de/HelpDialog.js
- *  
+ *
  *  Copyright (c) 2009-2013 The MathJax Consortium
  *
- *  Part of the MathJax library.
- *  See http://www.mathjax.org for details.
- * 
- *  Licensed under the Apache License, Version 2.0;
+ *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
-MathJax.Localization.addTranslation("de","HelpDialog",{version:"2.2",isLoaded:true,strings:{Help:"MathJax Hilfe",MathJax:"*MathJax* ist eine JavaScript-Bibliothek, die Autoren erlaubt, Ihren Webseiten mathematische Inhalte hinzuzuf\u00FCgen. Als Besucher m\u00FCssen sie nichts zus\u00E4tzliches tun, damit MathJax funktioniert.",Browsers:"*Browser*: MathJax ist kompatibel zu allen modernen Webbrowsern inklusive IE6+, Firefox 3+, Chrome 0.2+, Safari 2+, Opera 9.6+ und g\u00E4ngigen mobilen Browsern.",Menu:"*Mathe Men\u00FC*: MathJax f\u00FCgt ein Kontextmen\u00FC bei allen Formeln hinzu. Es wird mit Rechtsklick oder STRG+Linksklick auf einer Formel aufgerufen.",ShowMath:"*Zeige Mathe als* erlaubt es, eine Formel im Quellformat anzuzeigen, um Kopieren und Einf\u00FCgen (als MathML oder im Originalformat) zu erm00F6glichen.",Settings:"*Einstellungen* erlabut es, das Verhalten von MathJax zu modifizieren, so z.B. die Gr\u00F6\u00DFe der Mathematik sowie den Ausgabemechanismus.",Language:"*Sprache* erlaubt es, die Sprache zu wechseln, die MathJax  im Men\u00FC und den Warnmeldungen verwendent.",Zoom:"*Zoom*: Falls das Lesen der Formeln schwer f\u00E4llt, kann MathJax diese vergr\u00F6\u00DFern, um es zu erleichtern.",Accessibilty:"*Barrierfreiheit*: MathJax arbeite automatisch mit g\u00E4ngigen Screenreadern zusammen, um Mathematik barrierefrei darzustellen.",Fonts:"*Fonts*: MathJax benutzt gewisse mathematische Fonts, falls sie auf dem Systeminstalliert sind; ansonsten verwendet es Webfonts. Obwohl nicht notwendig, k\u00F6nnen installierte Fonts den Textsatz beschleunigen. Wir empfehlen, die [STIX fonts](%1) zu installieren."}});MathJax.Ajax.loadComplete("[MathJax]/localization/de/HelpDialog.js");
-
+MathJax.Localization.addTranslation("de","HelpDialog",{version:"2.3",isLoaded:true,strings:{Help:"MathJax Hilfe",MathJax:"*MathJax* ist eine JavaScript-Bibliothek, die es Seitenautoren erm\u00F6glicht, mathematische Inhalte in ihre Webseiten einzubinden. Als Leser muss du nichts unternehmen, um das zu erreichen.",Browsers:"*Browser*: MathJax funktioniert mit allen modernen Browsern inklusive Internet Explorer 6+, Firefox 3+, Chrome 0.2+, Safari 2+, Opera 9.6+ und den meisten mobilen Browsern.",Menu:"*Men\u00FC*: MathJax f\u00FCgt bei Gleichungen ein Kontextmen\u00FC hinzu. Um das Men\u00FC anzuzeigen, klicke mit der rechten Maustaste oder halte beim Klicken die Strg-Taste gedr\u00FCckt.",ShowMath:"*Inhalt zeigen als* erlaubt dir, das Formelquellenmarkup f\u00FCr Kopieren \u0026 Einf\u00FCgen anzusehen (als MathML oder im Originalformat).",Settings:"*Einstellungen* gibt dir die Kontrolle \u00FCber MathJax-Funktionen, wie die Inhaltsgr\u00F6\u00DFe und den verwendeten Mechanismus zum Anzeigen von Gleichungen.",Language:"*Sprache* l\u00E4sst dir die Sprache ausw\u00E4hlen, die von MathJax f\u00FCr die Men\u00FCs und Warnmeldungen verwendet wird.",Zoom:"*Zoom*: Wenn du beim Lesen einer Gleichung Schwierigkeiten hast, kann MathJax sie vergr\u00F6\u00DFern, damit du sie besser sehen kannst.",Accessibilty:"*Barrierefreiheit*: MathJax funktioniert automatisch mit Screenreadern, um die Inhalte f\u00FCr Sehbehinderte verf\u00FCgbar zu machen.",Fonts:"*Schriftarten*: MathJax verwendet bestimmte Schriften, wenn sie auf deinem Computer installiert sind. Anderenfalls werden webbasierte Schriften verwendet. Obwohl das nicht erforderlich ist, beschleunigen lokal installierte Schriften die Schriftsetzung. Wir empfehlen die Installation der [STIX-Schriften](%1)."}});MathJax.Ajax.loadComplete("[MathJax]/localization/de/HelpDialog.js");
