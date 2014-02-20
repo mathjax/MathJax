f6bc6fa129103b6969057e8b83a2bc95651d44bf
Davide P. Cervone <dpvc@union.edu>
2014-02-19 19:17:28 -0500

    Make \\left...\\right be of TeX class INNER.  Resolves issue #746.

--- unpacked/jax/input/TeX/jax.js
+++ unpacked/jax/input/TeX/jax.js
@@ -2130,8 +2130,7 @@
      *  Create an mrow that has stretchy delimiters at either end, as needed
      */
     fenced: function (open,mml,close) {
-      var mrow = MML.mrow();
-      mrow.open = open; mrow.close = close;
+      var mrow = MML.mrow().With({open:open, close:close, texClass:MML.TEXCLASS.INNER});
       if (open) {mrow.Append(MML.mo(open).With({fence:true, stretchy:true, texClass:MML.TEXCLASS.OPEN}))}
       if (mml.type === "mrow") {mrow.Append.apply(mrow,mml.data)} else {mrow.Append(mml)}
       if (close) {mrow.Append(MML.mo(close).With({fence:true, stretchy:true, texClass:MML.TEXCLASS.CLOSE}))}
