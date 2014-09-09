871f395ce65a6758255c428a096798d53f8bded6
Davide P. Cervone <dpvc@union.edu>
2014-09-09 11:27:39 -0400

    Make mrows that are embellished operators properly include the elements other than the core when stretched.  Resolves issue #775.

--- unpacked/jax/output/SVG/jax.js
+++ unpacked/jax/output/SVG/jax.js
@@ -1641,10 +1641,11 @@
 
     MML.mrow.Augment({
       SVG: BBOX.ROW,
-      toSVG: function () {
+      toSVG: function (h,d) {
         this.SVGgetStyles();
 	var svg = this.SVG();
         this.SVGhandleSpace(svg);
+        if (d != null) {this.sh = h; this.sd = d}
 	for (var i = 0, m = this.data.length; i < m; i++)
           {if (this.data[i]) {svg.Check(this.data[i])}}
         svg.Stretch(); svg.Clean();
@@ -1664,16 +1665,18 @@
       },
       SVGmultiline: function (span) {MML.mbase.SVGautoloadFile("multiline")},
       SVGstretchH: function (w) {
-	var svg = this.data[this.core].SVGstretchH(w);
-	this.SVGhandleColor(svg);
+	var svg = this.SVG(), child;
+        this.SVGhandleSpace(svg);
+	for (var i = 0, m = this.data.length; i < m; i++) {
+          if (!this.data[i]) continue;
+          if (i === this.core) {child = this.data[i].SVGstretchH(w)}
+            else {child = this.data[i].toSVG()}
+          svg.Add(child,svg.w,0);
+        }
+        svg.Clean();
+        this.SVGhandleColor(svg);
         this.SVGsaveData(svg);
-	return svg;
-      },
-      SVGstretchV: function (h,d) {
-	var svg = this.data[this.core].SVGstretchV(h,d);
-	this.SVGhandleColor(svg);
-        this.SVGsaveData(svg);
-	return svg;
+        return svg;
       }
     });
 
