9f14997b79653e5ee47da33a7c532e787165cfcc
Davide P. Cervone <dpvc@union.edu>
2015-01-17 08:25:14 -0500

    Make \overline{\lim_{x}} have correct spacing for overline with Firefox.  Issue #490.

--- unpacked/jax/input/TeX/jax.js
+++ unpacked/jax/input/TeX/jax.js
@@ -1430,8 +1430,10 @@
       var pos = {o: "over", u: "under"}[name.charAt(1)];
       var base = this.ParseArg(name);
       if (base.Get("movablelimits")) {base.movablelimits = false}
-      if (base.isa(MML.munderover) && base.isEmbellished())
-        {base = MML.mrow(MML.mo().With({rspace:0}),base)}  // add an empty <mi> so it's not embellished any more
+      if (base.isa(MML.munderover) && base.isEmbellished()) {
+        base.Core().With({lspace:0,rspace:0}); // get spacing right for NativeMML
+        base = MML.mrow(MML.mo().With({rspace:0}),base);  // add an empty <mi> so it's not embellished any more
+      }
       var mml = MML.munderover(base,null,null);
       mml.SetData(
         mml[pos], 
