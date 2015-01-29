a9dbd626287964704f090847b6bf08397f5d994e
Davide P. Cervone <dpvc@union.edu>
2015-01-29 11:02:39 -0500

    Repack and recombine for v2.5

--- jax/output/SVG/autoload/ms.js
+++ jax/output/SVG/autoload/ms.js
@@ -1,7 +1,7 @@
 /*
  *  /MathJax/jax/output/SVG/autoload/ms.js
  *
- *  Copyright (c) 2009-2014 The MathJax Consortium
+ *  Copyright (c) 2009-2015 The MathJax Consortium
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,4 +16,4 @@
  *  limitations under the License.
  */
 
-MathJax.Hub.Register.StartupHook("SVG Jax Ready",function(){var b="2.4.0";var a=MathJax.ElementJax.mml,c=MathJax.OutputJax.SVG;a.ms.Augment({toSVG:function(){this.SVGgetStyles();var e=this.SVG();this.SVGhandleSpace(e);var d=this.getValues("lquote","rquote");var f=this.SVGgetVariant(),h=this.SVGgetScale();var g=this.data.join("");e.Add(this.SVGhandleVariant(f,h,d.lquote+g+d.rquote));e.Clean();this.SVGhandleColor(e);this.SVGsaveData(e);return e}});a.ms.prototype.defaults.mathvariant="monospace";MathJax.Hub.Startup.signal.Post("SVG ms Ready");MathJax.Ajax.loadComplete(c.autoloadDir+"/ms.js")});
+MathJax.Hub.Register.StartupHook("SVG Jax Ready",function(){var b="2.5.0";var a=MathJax.ElementJax.mml,c=MathJax.OutputJax.SVG;a.ms.Augment({toSVG:function(){this.SVGgetStyles();var e=this.SVG();this.SVGhandleSpace(e);var d=this.getValues("lquote","rquote");var f=this.SVGgetVariant(),h=this.SVGgetScale();var g=this.data.join("");e.Add(this.SVGhandleVariant(f,h,d.lquote+g+d.rquote));e.Clean();this.SVGhandleColor(e);this.SVGsaveData(e);return e}});a.ms.prototype.defaults.mathvariant="monospace";MathJax.Hub.Startup.signal.Post("SVG ms Ready");MathJax.Ajax.loadComplete(c.autoloadDir+"/ms.js")});
