72af812dfe4e3e711f94103604be7d11362c0342
Davide P. Cervone <dpvc@union.edu>
2015-04-30 17:13:44 -0400

    Changed the wrong version number in the previous commit.

--- unpacked/jax/output/HTML-CSS/config.js
+++ unpacked/jax/output/HTML-CSS/config.js
@@ -27,7 +27,7 @@
 
 MathJax.OutputJax["HTML-CSS"] = MathJax.OutputJax({
   id: "HTML-CSS",
-  version: "2.5.2",
+  version: "2.5.3",
   directory: MathJax.OutputJax.directory + "/HTML-CSS",
   extensionDir: MathJax.OutputJax.extensionDir + "/HTML-CSS",
   autoloadDir: MathJax.OutputJax.directory + "/HTML-CSS/autoload",
