e770bdc4e64fe3320eb108b18e74ce7e8c2354e0
Omar Al-Ithawi <i@omardo.com>
2015-10-01 18:38:42 +0300

    WIP..2

--- rtl_test/rtl_config.js
+++ rtl_test/rtl_config.js
@@ -1,11 +1,11 @@
-;(function () {
+;
+(function() {
     var ArabicMathFont = 'Amiri'; // TODO: Make this configurable
 
     var macros = {
-        base: {
-        },
+        base: {},
         translatable: [],
-        add: function (command, en, ar) {
+        add: function(command, en, ar) {
             this.translatable.push({
                 command: command,
                 en: en,
@@ -15,31 +15,31 @@
             return this;
         },
         // Add Arabic text
-        addat: function (command, en, arabicText) {
+        addat: function(command, en, arabicText) {
             var ar = '\\fliph{\\text{' + arabicText + '}}';
             return this.add(command, en, ar);
         },
-        addas: function (command, en, arabicSymb) {
+        addas: function(command, en, arabicSymb) {
             // TODO: Implement Arabic symobls method
         },
-        build: function () {
+        build: function() {
             var baseMacros = this.base;
             var macros = {};
 
-            Object.keys(baseMacros).forEach(function (cmd) {
+            Object.keys(baseMacros).forEach(function(cmd) {
                 macros[cmd] = baseMacros[cmd];
             });
 
-            this.translatable.forEach(function (cmd) {
+            this.translatable.forEach(function(cmd) {
                 macros[cmd.command] = cmd.en;
             });
 
             return macros;
         },
-        buildArabic: function () {
+        buildArabic: function() {
             var commands = [];
 
-            this.translatable.forEach(function (cmd) {
+            this.translatable.forEach(function(cmd) {
                 var ar = [
                     '\\renewcommand{',
                     '\\', cmd.command,
@@ -55,6 +55,7 @@
         }
     };
 
+    // TODO: Convert to TeX commands using `ArabicText` and alike
     macros.addat('lspeed', 'c', 'سرعة الضوء')
         .addat('Planck', '\\hbar', 'ثابت بلانك')
         .addat('Boltzmann', 'k', 'ثابت بولتزمان')
@@ -120,43 +121,33 @@
         },
         TeX: {
             // TODO: Review the dependecies!
-            extensions: ["newcommand.js", "HTML.js"],
+            extensions: ['newcommand.js', 'HTML.js'],
             Macros: {
                 'fliph': ['\\class{mfliph}{#1}', 1]
             }
         }
     });
 
-    // MathJax.Hub.Register.StartupHook('TeX newcommand Ready', function () {
-    //     // Makes it easier if you want to do several definitions
-    //     var TEX = MathJax.InputJax.TeX;
-    //     TEX.Environment(
-    //         'ar',
-    //         '\\require{begingroup} \\begingroup ' + macros.buildArabic(),
-    //         '\\endgroup'
-    //     );
-    // });
-
-    MathJax.Hub.Register.StartupHook("TeX HTML Ready", function () {
+    MathJax.Hub.Register.StartupHook('TeX HTML Ready', function() {
         var TEX = MathJax.InputJax.TeX;
-
         var TEXDEF = TEX.Definitions;
 
         TEXDEF.Add({
             macros: {
                 'ar': 'HandleArabic',
-                'alwaysar': 'HandleAlwaysArabic',
+                'alwaysar': 'MarkAsArabic',
                 'lspeed': 'LightSpeedAr',
                 'scnd': 'SecondsAr',
                 'Planck': 'PlancksAr',
                 'Boltzmann': 'BoltzmannsAr',
+                'zero': 'ZeroAr',
             }
         });
 
-        var ArabicText = function (english, arabicText) {
-            return function (name) {
+        var ArabicText = function(english, arabicText) {
+            return function(name) {
                 var tex;
-                if (this.stack.env.isArabic) {
+                if ('ar' === this.stack.env.lang) {
                     tex = '\\fliph{\\text{' + arabicText + '}}';
                 } else {
                     tex = english;
@@ -166,208 +157,198 @@
             };
         };
 
-
-        // TODO: Remove this hack
-        // MathJax.InputJax.TeX.postfilterHooks.Add(function (data) {
-        //     data.math.root.class = 'mfliph'; // Flip the root element
-        //     window.omardo = data.math.root;
-        // });
+        var texParseMMLToken = TEX.Parse.prototype.mmlToken;
 
         TEX.Parse.Augment({
-            HandleArabic: function (name) {
-                // TODO: Make this configurable
+            HandleArabic: function(name) {
+                // TODO: Make the `documentElement.lang` check configurable
                 var pageLang = document.documentElement.lang;
                 if (pageLang === 'ar') {
-                    this.HandleAlwaysArabic(name);
+                    this.MarkAsArabic(name);
                 }
             },
-            HandleAlwaysArabic: function (name) {
-                // TODO: Consider changing this to `this.stack.env.language`
-                // For more modular work
-                this.stack.env.isArabic = true;
+            MarkAsArabic: function(name) {
+                this.stack.env.lang = 'ar';
 
                 var CLASS = 'mfliph';
 
                 var arg = this.GetArgumentMML(name);
 
-                if (arg["class"] != null) {
-                    CLASS = arg["class"] + " " + CLASS
+                if (arg['class'] != null) {
+                    CLASS = arg['class'] + ' ' + CLASS
                 }
 
-                this.Push(arg.With({"class":CLASS}));
+                var arMML = arg.With({
+                    'class': CLASS,
+                    'lang': 'ar'
+                });
+
+                this.Push(arMML);
+            },
+            mapArabicNumbers: function(string) {
+                var numbers = [
+                    '٠',  // 0
+                    '١',  // 1
+                    '٢',  // 2
+                    '٣',  // 3
+                    '٤',  // 4
+                    '٥',  // 5
+                    '٦',  // 6
+                    '٧',  // 7
+                    '٨',  // 8
+                    '٩'  // 9
+                ];
+
+                numbers.forEach(function (hindiNumber, arabicNumber) {
+                    var regex = new RegExp('' + arabicNumber, 'g');
+                    string = string.replace(regex, hindiNumber);
+                });
+
+                return string;
+            },
+            mapArabicChars: function(string) {
+                // TODO: Move this function to the HTML-CSS OutputJax
+
+                // TODO: Perhaps even this should be configurable!
+                var enToArChars = {
+                    // Variable naem
+                    'a': 'ا',
+
+                    // Variable naem
+                    // TODO: Use Arabic letter dotless beh 0x66e instead
+                    'b': 'ب',
+
+                    // Variable naem.
+                    // Suffixed with Unicdoe Arabic tatweel 0x0640
+                    'c': 'حـ',
+
+                    // Mixed use (Function, variable and (dx))
+                    'd': 'د',
+
+                    // Mixed use. With Unicdoe Arabic tatweel 0x0640
+                    'e': 'هـ',
+
+                    // Energy
+                    'E': 'ط',
+
+                    // Function name
+                    // TODO: Use dotless qaf (ٯ) instead
+                    'f': 'ق',
+
+                    // Function name
+                    'g': 'د',
+
+                    // Mixed use
+                    'k': 'ك',
+
+                    // Variable name
+                    'n': 'ن',
+
+                    // Meter
+                    'm': 'م',
+
+                    // Initial charge
+                    'q': 'ش',
+
+                    // Mixed use
+                    'r': 'ر',
+                    't': 'ت',
+
+                    // Variable names
+                    'x': 'س',
+                    'y': 'ص',
+                    'z': 'ع'
+                };
+
+                for (var enChar in enToArChars) {
+                    if (enToArChars.hasOwnProperty(enChar)) {
+                        var arChar = enToArChars[enChar];
+                        var regex = new RegExp(enChar, 'g');
+                        string = string.replace(regex, arChar);
+                    }
+                }
+
+                return string;
+            },
+            arabicNumber: function (token) {
+                // TODO: Move this function to the HTML-CSS OutputJax
+
+                token.class = token.class || '';
+                token.class += ' ' + 'mfliph mar';
+
+                var textContent = token.data[0].data[0];
+
+                token['class'] = 'mfliph mar';
+
+                if ('0' === textContent) {
+                    // Special case for the Arabic zero
+                    // TODO: Not sure if this is a good idea to
+                    // convert the type!
+
+                    token.type = 'mi';
+                    token._originalType = 'mo';
+                    token.data[0].data[0] = 'صفر';
+                } else {
+                    var arabicNumbers = this.mapArabicNumbers(textContent);
+                    token.data[0].data[0] = arabicNumbers;
+                }
+
+                return token;
+            },
+            arabicIdentifier: function (token) {
+                // TODO: Move this function to the HTML-CSS OutputJax
+
+                // English Symbols
+                if ('chars' === token.data[0].type) {
+                    var textContent = token.data[0].data[0];
+                    var mapped = this.mapArabicChars(textContent);
+
+                    // TODO: Handle entities like PI
+                    if (mapped != textContent) {
+                        token['class'] = ' mfliphchild marchild';
+                        token.data[0].data[0] = mapped;
+                    }
+                } else if ('entity' === token.data[0].type) {
+                    token['class'] = ' mfliph';
+                }
+
+                return token;
+            },
+            mmlToken: function (token) {
+                // TODO: Move some of this function to the HTML-CSS OutputJax
+                // TODO: Check for possible incomability with boldsymbol
+                // extension
+                var token = texParseMMLToken.call(this, token);
+
+                if ('ar' === this.stack.env.lang) {
+                    if ('mn' === token.type) {
+                        token = this.arabicNumber(token);
+                    } else if ('mi' === token.type) {
+                        token = this.arabicIdentifier(token);
+                    }
+                }
+
+                return token;
             },
             LightSpeedAr: ArabicText('c', 'سرعة الضوء'),
             SecondsAr: ArabicText('s', 'ث'),
             PlancksAr: ArabicText('\\hbar', 'ثابت بلانك'),
             BoltzmannsAr: ArabicText('k', 'ثابت بولتزمان'),
+            // EpsilonZeroAr: ArabicText('\\varepsilon\\zero', '\\varepsilon\\zero'),
         });
     });
 
-    // MathJax.Hub.Register.StartupHook("TeX Jax Ready", function () {
-    //     MathJax.InputJax.TeX.postfilterHooks.Add(function (data) {
-    //         var rawMath = data.script.innerHTML;
+    // MathJax.Hub.Register.StartupHook('mml Jax Ready', function() {
+    //     MathJax.Hub.Register.StartupHook('HTML-CSS Jax Ready', function() {
+    //         var MML = MathJax.ElementJax.mml;
     //
-    //         // TODO: Change this into environment check
-    //         var isConditionalArabic = (rawMath.indexOf('\\begin{ar}') > -1);
-    //
-    //         var isAlwaysArabic = (rawMath.indexOf('\\alwaysar') > -1);
-    //
-    //         var pageLang = document.documentElement.lang;
-    //
-    //         var doMakeArabic = (
-    //             // Make `pageLang === 'ar'` configurable
-    //             isAlwaysArabic || (isConditionalArabic && pageLang === 'ar')
-    //         );
-    //
-    //         if (!doMakeArabic) {
-    //             return data.math;
-    //         }
-    //
-    //         var mapNumbers = function(string) {
-    //             var numbers = [
-    //                 '٠',  // 0
-    //                 '١',  // 1
-    //                 '٢',  // 2
-    //                 '٣',  // 3
-    //                 '٤',  // 4
-    //                 '٥',  // 5
-    //                 '٦',  // 6
-    //                 '٧',  // 7
-    //                 '٨',  // 8
-    //                 '٩'  // 9
-    //             ];
-    //
-    //             if ('0' === string) {
-    //                 // Special case for the Arabic zero
-    //                 return 'صفر';
+    //         var miToHTML = MML.mi.prototype.toHTML;
+    //         MML.mi.Augment({
+    //             toHTML: function(span) {
+    //                 var element = miToHTML.call(this, span);
+    //                 return element;
     //             }
-    //
-    //             numbers.forEach(function (hindiNumber, arabicNumber) {
-    //                 var regex = new RegExp('' + arabicNumber, 'g');
-    //                 string = string.replace(regex, hindiNumber);
-    //             });
-    //
-    //             return string;
-    //         };
-    //
-    //         var mapChars = function(string) {
-    //             // Perhaps even this should be configurable!
-    //             var enToArChars = {
-    //                 // Variable naem
-    //                 'a': 'ا',
-    //
-    //                 // Variable naem
-    //                 // TODO: Use Arabic letter dotless beh 0x66e instead
-    //                 'b': 'ب',
-    //
-    //                 // Variable naem.
-    //                 // Suffixed with Unicdoe Arabic tatweel 0x0640
-    //                 'c': 'حـ',
-    //
-    //                 // Mixed use (Function, variable and (dx))
-    //                 'd': 'د',
-    //
-    //                 // Mixed use. With Unicdoe Arabic tatweel 0x0640
-    //                 'e': 'هـ',
-    //
-    //                 // Energy
-    //                 'E': 'ط',
-    //
-    //                 // Function name
-    //                 // TODO: Use dotless qaf (ٯ) instead
-    //                 'f': 'ق',
-    //
-    //                 // Function name
-    //                 'g': 'د',
-    //
-    //                 // Mixed use
-    //                 'k': 'ك',
-    //
-    //                 // Variable name
-    //                 'n': 'ن',
-    //
-    //                 // Meter
-    //                 'm': 'م',
-    //
-    //                 // Initial charge
-    //                 'q': 'ش',
-    //
-    //                 // Mixed use
-    //                 'r': 'ر',
-    //                 't': 'ت',
-    //
-    //                 // Variable names
-    //                 'x': 'س',
-    //                 'y': 'ص',
-    //                 'z': 'ع'
-    //             };
-    //
-    //             for (var enChar in enToArChars) {
-    //                 if (enToArChars.hasOwnProperty(enChar)) {
-    //                     var arChar = enToArChars[enChar];
-    //                     var regex = new RegExp(enChar, 'g');
-    //                     string = string.replace(regex, arChar);
-    //                 }
-    //             }
-    //
-    //             return string;
-    //         };
-    //
-    //         var markRTL = function(node) {
-    //             if (!node) return;
-    //
-    //             if ('mi' === node.type) {
-    //                 // English Symbols
-    //                 if ('chars' === node.data[0].type) {
-    //                     var textContent = node.data[0].data[0];
-    //                     var mapped = mapChars(textContent);
-    //
-    //                     // TODO: Handle entities like PI
-    //                     if (mapped != textContent) {
-    //                         node['class'] = ' mfliphchild marchild';
-    //                         node.data[0].data[0] = mapped;
-    //                     }
-    //                 } else if ('entity' === node.data[0].type) {
-    //                     node['class'] = ' mfliph';
-    //                 }
-    //             } else if ('mn' === node.type) {
-    //                 // Numbers
-    //                 var textContent = node.data[0].data[0];
-    //
-    //                 node['class'] = 'mfliphchild marchild';
-    //
-    //                 if ('0' === textContent) {
-    //                     node.type = 'mi'; // TODO: Not sure if this is a good idea!
-    //                 }
-    //
-    //                 node.data[0].data[0] = mapNumbers(textContent);
-    //             }
-    //         }
-    //
-    //         var traverseMML = function(node, func) {
-    //             try {
-    //                 if (!node) {
-    //                     return;
-    //                 }
-    //
-    //                 func(node);
-    //
-    //                 if (node.data) {
-    //                     node.data.forEach(function (subNode) {
-    //                         // Going on step down in the object tree!!
-    //                         traverseMML(subNode, func);
-    //                     });
-    //                 }
-    //             } catch (e) {
-    //                 console.error(e);
-    //             }
-    //         }
-    //
-    //         data.math.root.class = 'mfliph'; // Flip the root element
-    //         traverseMML(data.math.root, markRTL);
-    //
-    //         return data.math;
+    //         });
     //     });
     // });
-
 }());
