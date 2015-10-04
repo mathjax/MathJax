fe158a57d719c98cfad1cbbff51f6cbcb0fdf555
Omar Al-Ithawi <i@omardo.com>
2015-10-04 16:59:39 +0300

    Completed the implementaion of so many things!

--- rtl_test/rtl_config.js
+++ rtl_test/rtl_config.js
@@ -2,110 +2,12 @@
 (function() {
     var ArabicMathFont = 'Amiri'; // TODO: Make this configurable
 
-    var macros = {
-        base: {},
-        translatable: [],
-        add: function(command, en, ar) {
-            this.translatable.push({
-                command: command,
-                en: en,
-                ar: ar
-            });
-
-            return this;
-        },
-        // Add Arabic text
-        addat: function(command, en, arabicText) {
-            var ar = '\\fliph{\\text{' + arabicText + '}}';
-            return this.add(command, en, ar);
-        },
-        addas: function(command, en, arabicSymb) {
-            // TODO: Implement Arabic symobls method
-        },
-        build: function() {
-            var baseMacros = this.base;
-            var macros = {};
-
-            Object.keys(baseMacros).forEach(function(cmd) {
-                macros[cmd] = baseMacros[cmd];
-            });
-
-            this.translatable.forEach(function(cmd) {
-                macros[cmd.command] = cmd.en;
-            });
-
-            return macros;
-        },
-        buildArabic: function() {
-            var commands = [];
-
-            this.translatable.forEach(function(cmd) {
-                var ar = [
-                    '\\renewcommand{',
-                    '\\', cmd.command,
-                    '}{',
-                    cmd.ar,
-                    '}',
-                ];
-
-                commands.push(ar.join(''));
-            });
-
-            return commands.join('\n');
-        }
-    };
-
-    // TODO: Convert to TeX commands using `ArabicText` and alike
-    macros.addat('lspeed', 'c', 'سرعة الضوء')
-        .addat('Planck', '\\hbar', 'ثابت بلانك')
-        .addat('Boltzmann', 'k', 'ثابت بولتزمان')
-        .addat('zero', '0', '0')
-        // TODO: Refactor to `addas` method to call it with the
-        // Arabic variable: .addas('rad', 'l', 'ع')
-        .add('rad', 'l', 'z') // z --> ع
-        .addat('AM', 'AM', 'كتلة هواء')
-        .addat('Mega', 'M', 'ميجا')
-        .addat('nano', 'n', 'نانو')
-        .addat('Giga', 'G', 'جيجا')
-        .addat('kilo', 'k', 'كيلو')
-        .addat('volt', 'v', 'فولت')
-        .addat('Amp', 'A', 'امبير')
-        .addat('micro', '\\mu', 'مايكرو')
-        .add('Tmpr', 'T', 'd') // d --> د
-        .addat('Klvn', 'K', 'كلفن')
-        .addat('cm', '\\text{cm}', 'سم')
-        .addat('scnd', 's', 'ث')
-        .add('current', 'I', 't') // t --> ت
-        .add('er', 'ϵr', '\\fliph{ϵr}')
-        .addat('J', 'J', 'ك.ت')
-        .addat('ph', 'ph', 'ف')
-        .add('Voc', 'V_{oc}', 'c_m')
-        .add('NA', 'NA', 'n_f') // n --> ن, f --> ق
-        .add('ND', 'ND', 'n_m') // n --> ن, m --> م
-        .add('nii', 'ni', 'n_k') // n --> ن, k --> ك
-        .add('Wd', 'W', 'l_n') // l --> ل, n --> ن
-        .addat('F', 'F', 'فاراد')
-        .add('Ld', 'Ld', 'l_r') // l --> ل, r --> ر
-        .add('D', 'D', 'm')
-        .addat('ek', 'e', 'انتشار،ك')
-        .addat('hole', 'p', 'ثقب')
-        .addat('elctrn', 'n', 'الكترون')
-        .addat('hour', 'h', 'ساعة')
-        .addat('Watt', 'W', 'واط')
-        .addat('max', 'p', 'اقصى')
-        .addat('day', '\\text{day}', 'يوم')
-        .addat('year', '\\text{year}', 'سنة')
-        .add('sc', 'sc', 'f') // f --> ق
-        .addat('FF', 'FF', 'ع.ت')
-        .add('P', 'P', 't') // t --> ط
-        .addat('inn', 'in', 'd'); // d --> د
-
     MathJax.Hub.Config({
         'HTML-CSS': {
             undefinedFamily: ArabicMathFont,
         },
         styles: {
-            '.mfliph, .mfliphchild > span': {
+            '.mfliph': {
                 'display': 'inline-block !important',
                 '-moz-transform': 'scaleX(-1)',
                 '-webkit-transform': 'scaleX(-1)',
@@ -114,17 +16,14 @@
                 '-ms-filter': 'fliph',
                 'filter': 'fliph'
             },
-            '.mar, .marchild > span': {
+            '.mar': {
                 'font-family': ArabicMathFont + ' !important',
-                'font-style': 'normal !important'
-            }
+                'font-style': 'normal !important',
+            },
         },
         TeX: {
             // TODO: Review the dependecies!
-            extensions: ['newcommand.js', 'HTML.js'],
-            Macros: {
-                'fliph': ['\\class{mfliph}{#1}', 1]
-            }
+            extensions: ['HTML.js'],
         }
     });
 
@@ -136,19 +35,128 @@
             macros: {
                 'ar': 'HandleArabic',
                 'alwaysar': 'MarkAsArabic',
+                'fliph': 'FlipHorizontal',
+
                 'lspeed': 'LightSpeedAr',
                 'scnd': 'SecondsAr',
                 'Planck': 'PlancksAr',
                 'Boltzmann': 'BoltzmannsAr',
                 'zero': 'ZeroAr',
+                'varepszero': 'EpsilonZeroAr',
+
+                'AM': 'AirMassAr',
+                'Mega': 'MegaAr',
+                'nano': 'NanoAr',
+                'Giga': 'GigaAr',
+                'kilo': 'KiloAr',
+                'volt': 'VoltAr',
+                'Amp': 'AmpAr',
+                'Klvn': 'KilvenAr',
+                'ph': 'PHAr',
+                'F': 'FaradAr',
+                'ek': 'EKAr',
+                'hole': 'HoleAr',
+                'elctrn': 'ElectronAr',
+                'hour': 'HourAr',
+                'Watt': 'WattAr',
+                'max': 'MaxAr',
+
+                'micro': 'MicroAr',
+                'cm': 'CentimeterAr',
+                'day': 'DayAr',
+                'year': 'YearAr',
+
+                'er': 'EspsilonRAr',
+
+                'J': 'CurrentAr',
+                'FF': 'FillFactorAr',
+                'Voc': 'VoltageOpenCircuitAr',
+                'D': 'SpreadCoefficientAr',
+                'rad': 'RadiationAr',
+                'Tmpr': 'TemratureAr',
+                'current': 'CurrentAr',
+                'NA': 'ConcentrationReceiverAtomAr',
+                'ND': 'ConcentrationDonorAtomAr',
+                'nii': 'ConcentrationCarierPureAr',
+                'Wd': 'DeplationAreaWidthAr',
+                'Ld': 'DiffusionLengthAr',
+                'sc': 'ShortCircuitAr',
+
+                // Light power
+                'P': 'PowerAr',
+                'inn': 'INAr',
             }
         });
 
-        var ArabicText = function(english, arabicText) {
+        var NUMBERS_MAP = [
+            '٠',  // 0
+            '١',  // 1
+            '٢',  // 2
+            '٣',  // 3
+            '٤',  // 4
+            '٥',  // 5
+            '٦',  // 6
+            '٧',  // 7
+            '٨',  // 8
+            '٩'  // 9
+        ];
+
+        // TODO: Perhaps even this should be configurable!
+        var SYMBOLS_MAP = {
+            // Variable naem
+            'a': 'ا',
+
+            // Variable naem
+            // TODO: Use Arabic letter dotless beh 0x66e instead
+            'b': 'ب',
+
+            // Variable naem.
+            // Suffixed with Unicdoe Arabic tatweel 0x0640
+            'c': 'حـ',
+
+            // Mixed use (Function, variable and (dx))
+            'd': 'د',
+
+            // Mixed use. With Unicdoe Arabic tatweel 0x0640
+            'e': 'هـ',
+
+            // Energy
+            'E': 'ط',
+
+            // Function name
+            // TODO: Use dotless qaf (ٯ) instead
+            'f': 'ق',
+
+            // Function name
+            'g': 'د',
+
+            // Mixed use
+            'k': 'ك',
+
+            // Variable name
+            'n': 'ن',
+
+            // Meter
+            'm': 'م',
+
+            // Initial charge
+            'q': 'ش',
+
+            // Mixed use
+            'r': 'ر',
+            't': 'ت',
+
+            // Variable names
+            'x': 'س',
+            'y': 'ص',
+            'z': 'ع'
+        };
+
+        var ArabicTeX = function(english, arabic) {
             return function(name) {
                 var tex;
                 if ('ar' === this.stack.env.lang) {
-                    tex = '\\fliph{\\text{' + arabicText + '}}';
+                    tex = arabic;
                 } else {
                     tex = english;
                 }
@@ -156,6 +164,12 @@
                 this.Push(TEX.Parse(tex).mml());
             };
         };
+
+        var ArabicText = function(english, arabicText) {
+            return ArabicTeX(english, '\\fliph{\\text{' + arabicText + '}}');
+        };
+
+        var ArabicSymbols = ArabicTeX;
 
         var texParseMMLToken = TEX.Parse.prototype.mmlToken;
 
@@ -178,28 +192,19 @@
                     CLASS = arg['class'] + ' ' + CLASS
                 }
 
-                var arMML = arg.With({
+                this.Push(arg.With({
                     'class': CLASS,
                     'lang': 'ar'
-                });
-
-                this.Push(arMML);
+                }));
+            },
+            FlipHorizontal: function (name) {
+                var arg = this.GetArgumentMML(name);
+                this.Push(arg.With({
+                    'fliph': true,
+                }));
             },
             mapArabicNumbers: function(string) {
-                var numbers = [
-                    '٠',  // 0
-                    '١',  // 1
-                    '٢',  // 2
-                    '٣',  // 3
-                    '٤',  // 4
-                    '٥',  // 5
-                    '٦',  // 6
-                    '٧',  // 7
-                    '٨',  // 8
-                    '٩'  // 9
-                ];
-
-                numbers.forEach(function (hindiNumber, arabicNumber) {
+                NUMBERS_MAP.forEach(function (hindiNumber, arabicNumber) {
                     var regex = new RegExp('' + arabicNumber, 'g');
                     string = string.replace(regex, hindiNumber);
                 });
@@ -208,61 +213,9 @@
             },
             mapArabicChars: function(string) {
                 // TODO: Move this function to the HTML-CSS OutputJax
-
-                // TODO: Perhaps even this should be configurable!
-                var enToArChars = {
-                    // Variable naem
-                    'a': 'ا',
-
-                    // Variable naem
-                    // TODO: Use Arabic letter dotless beh 0x66e instead
-                    'b': 'ب',
-
-                    // Variable naem.
-                    // Suffixed with Unicdoe Arabic tatweel 0x0640
-                    'c': 'حـ',
-
-                    // Mixed use (Function, variable and (dx))
-                    'd': 'د',
-
-                    // Mixed use. With Unicdoe Arabic tatweel 0x0640
-                    'e': 'هـ',
-
-                    // Energy
-                    'E': 'ط',
-
-                    // Function name
-                    // TODO: Use dotless qaf (ٯ) instead
-                    'f': 'ق',
-
-                    // Function name
-                    'g': 'د',
-
-                    // Mixed use
-                    'k': 'ك',
-
-                    // Variable name
-                    'n': 'ن',
-
-                    // Meter
-                    'm': 'م',
-
-                    // Initial charge
-                    'q': 'ش',
-
-                    // Mixed use
-                    'r': 'ر',
-                    't': 'ت',
-
-                    // Variable names
-                    'x': 'س',
-                    'y': 'ص',
-                    'z': 'ع'
-                };
-
-                for (var enChar in enToArChars) {
-                    if (enToArChars.hasOwnProperty(enChar)) {
-                        var arChar = enToArChars[enChar];
+                for (var enChar in SYMBOLS_MAP) {
+                    if (SYMBOLS_MAP.hasOwnProperty(enChar)) {
+                        var arChar = SYMBOLS_MAP[enChar];
                         var regex = new RegExp(enChar, 'g');
                         string = string.replace(regex, arChar);
                     }
@@ -271,51 +224,44 @@
                 return string;
             },
             arabicNumber: function (token) {
-                // TODO: Move this function to the HTML-CSS OutputJax
-
-                token.class = token.class || '';
-                token.class += ' ' + 'mfliph mar';
-
                 var textContent = token.data[0].data[0];
-
-                token['class'] = 'mfliph mar';
 
                 if ('0' === textContent) {
                     // Special case for the Arabic zero
-                    // TODO: Not sure if this is a good idea to
-                    // convert the type!
-
-                    token.type = 'mi';
-                    token._originalType = 'mo';
                     token.data[0].data[0] = 'صفر';
                 } else {
                     var arabicNumbers = this.mapArabicNumbers(textContent);
                     token.data[0].data[0] = arabicNumbers;
                 }
 
-                return token;
+                return token.With({
+                    lang: 'ar',
+                    fliph: true
+                });
             },
             arabicIdentifier: function (token) {
-                // TODO: Move this function to the HTML-CSS OutputJax
-
                 // English Symbols
                 if ('chars' === token.data[0].type) {
                     var textContent = token.data[0].data[0];
                     var mapped = this.mapArabicChars(textContent);
 
-                    // TODO: Handle entities like PI
                     if (mapped != textContent) {
-                        token['class'] = ' mfliphchild marchild';
                         token.data[0].data[0] = mapped;
+
+                        token = token.With({
+                            lang: 'ar',
+                            fliph: true
+                        });
                     }
                 } else if ('entity' === token.data[0].type) {
-                    token['class'] = ' mfliph';
+                    token = token.With({
+                        fliph: true
+                    });
                 }
 
                 return token;
             },
             mmlToken: function (token) {
-                // TODO: Move some of this function to the HTML-CSS OutputJax
                 // TODO: Check for possible incomability with boldsymbol
                 // extension
                 var token = texParseMMLToken.call(this, token);
@@ -331,24 +277,114 @@
                 return token;
             },
             LightSpeedAr: ArabicText('c', 'سرعة الضوء'),
-            SecondsAr: ArabicText('s', 'ث'),
             PlancksAr: ArabicText('\\hbar', 'ثابت بلانك'),
             BoltzmannsAr: ArabicText('k', 'ثابت بولتزمان'),
-            // EpsilonZeroAr: ArabicText('\\varepsilon\\zero', '\\varepsilon\\zero'),
+            ZeroAr: ArabicText('0', '0'),
+            SecondsAr: ArabicText('s', 'ث'),
+            EpsilonZeroAr: ArabicTeX(
+                '\\varepsilon_\\zero',
+                '\\fliph{\\varepsilon_\\zero}'
+            ),
+            AirMassAr: ArabicText('AM', 'كتلة هواء'),
+            MegaAr: ArabicText('M', 'ميجا'),
+            NanoAr: ArabicText('n', 'نانو'),
+            GigaAr: ArabicText('G', 'جيجا'),
+            KiloAr: ArabicText('k', 'كيلو'),
+            VoltAr: ArabicText('v', 'فولت'),
+            AmpAr: ArabicText('A', 'امبير'),
+            KilvenAr: ArabicText('K', 'كلفن'),
+            PHAr: ArabicText('ph', 'ف'),
+            FaradAr: ArabicText('F', 'فاراد'),
+            EKAr: ArabicText('e', 'انتشار،ك'),
+            HoleAr: ArabicText('p', 'ثقب'),
+            ElectronAr: ArabicText('n', 'الكترون'),
+            HourAr: ArabicText('h', 'ساعة'),
+            WattAr: ArabicText('W', 'واط'),
+            MaxAr: ArabicText('p', 'اقصى'),
+
+            MicroAr: ArabicText('\\mu', 'مايكرو'),
+            CentimeterAr: ArabicText('\\text{cm}', 'سم'),
+            DayAr: ArabicText('\\text{day}', 'يوم'),
+            YearAr: ArabicText('\\text{year}', 'سنة'),
+
+            EspsilonRAr: ArabicTeX('ϵr', '\\fliph{ϵr}'),
+
+            // TODO: Enter the actual Arabic letters before the mapping
+            CurrentAr: ArabicSymbols('J', 'k.t'), // --> ك.ت
+            FillFactorAr: ArabicSymbols('FF', 'z.t'), // z.t --> ع.ت
+            VoltageOpenCircuitAr: ArabicSymbols('V_{oc}', 'c_m'), // c --> حـ, m --> م
+            SpreadCoefficientAr: ArabicSymbols('D', 'm'), // m --> م
+            RadiationAr: ArabicSymbols('l', 'z'), // z --> ع
+            TemratureAr: ArabicSymbols('T', 'd'), // d --> د
+            CurrentAr: ArabicSymbols('I', 't'), // t --> ت
+            ConcentrationReceiverAtomAr: ArabicSymbols('NA', 'n_f'), // n --> ن, f --> ق
+            ConcentrationDonorAtomAr: ArabicSymbols('ND', 'n_m'), // n --> ن, m --> م
+            ConcentrationCarierPureAr: ArabicSymbols('ni', 'n_k'), // n --> ن, k --> ك
+            DeplationAreaWidthAr: ArabicSymbols('W', 'l_n'), // l --> ل, n --> ن
+            DiffusionLengthAr: ArabicSymbols('Ld', 'l_r'), // l --> ل, r --> ر
+            ShortCircuitAr: ArabicSymbols('sc', 'f'), // f --> ق
+            PowerAr: ArabicSymbols('P', 't'), // t --> ط
+            INAr: ArabicSymbols('in', 'd') // d --> د
         });
     });
 
-    // MathJax.Hub.Register.StartupHook('mml Jax Ready', function() {
-    //     MathJax.Hub.Register.StartupHook('HTML-CSS Jax Ready', function() {
-    //         var MML = MathJax.ElementJax.mml;
-    //
-    //         var miToHTML = MML.mi.prototype.toHTML;
-    //         MML.mi.Augment({
-    //             toHTML: function(span) {
-    //                 var element = miToHTML.call(this, span);
-    //                 return element;
-    //             }
-    //         });
-    //     });
-    // });
+    // TODO: Move HTML-CSS specific stuff here instead of the TeX Jax
+    MathJax.Hub.Register.StartupHook('mml Jax Ready', function() {
+        MathJax.Hub.Register.StartupHook('HTML-CSS Jax Ready', function() {
+            var MML = MathJax.ElementJax.mml;
+
+            var mnToHTML = MML.mn.prototype.toHTML;
+
+            var flipHElement = function (token, element) {
+                if (token.Get('fliph')) {
+                    var flipElement = document.createElement('span');
+                    var className = 'mfliph';
+
+                    if ('ar' === token.Get('lang')) {
+                        className += ' ' + 'mar';
+                    }
+
+                    flipElement.className = className;
+
+                    if (Node.TEXT_NODE === element.firstChild.nodeType) {
+                        flipElement.textContent = element.textContent;
+                        element.textContent = '';
+                    } else {
+                        while (element.childNodes.length) {
+                            flipElement.appendChild(element.firstChild);
+                        }
+                    }
+
+                    element.appendChild(flipElement);
+                }
+            };
+
+            var miToHTML = MML.mi.prototype.toHTML;
+            MML.mi.Augment({
+                toHTML: function(span) {
+                    var element = miToHTML.call(this, span);
+
+                    if (Node.TEXT_NODE === element.firstChild.nodeType) {
+                        flipHElement(this, element);
+                    } else {
+                        flipHElement(this, element.firstChild);
+                    }
+
+                    return element;
+                }
+            });
+
+            ['mn', 'mtext', 'msubsup'].forEach(function (name) {
+                var originalToHTML = MML[name].prototype.toHTML;
+
+                MML[name].Augment({
+                    toHTML: function(span) {
+                        var element = originalToHTML.call(this, span);
+                        flipHElement(this, element);
+                        return element;
+                    }
+                });
+            });
+        });
+    });
 }());
