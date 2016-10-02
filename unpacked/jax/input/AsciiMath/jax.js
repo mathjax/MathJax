d2a570ded819de257db11072eca9b0c5cc5584ef
Davide P. Cervone <dpvc@union.edu>
2016-10-02 12:24:46 -0400

    Update AsciiMath to include changes since previous update

--- unpacked/jax/input/AsciiMath/jax.js
+++ unpacked/jax/input/AsciiMath/jax.js
@@ -546,6 +546,7 @@ var AMsymbols = [
 {input:"aleph", tag:"mo", output:"\u2135", tex:null, ttype:CONST},
 {input:"...",  tag:"mo", output:"...",    tex:"ldots", ttype:CONST},
 {input:":.",  tag:"mo", output:"\u2234",  tex:"therefore", ttype:CONST},
+{input:":'",  tag:"mo", output:"\u2235",  tex:"because", ttype:CONST},
 {input:"/_",  tag:"mo", output:"\u2220",  tex:"angle", ttype:CONST},
 {input:"/_\\",  tag:"mo", output:"\u25B3",  tex:"triangle", ttype:CONST},
 {input:"'",   tag:"mo", output:"\u2032",  tex:"prime", ttype:CONST},
