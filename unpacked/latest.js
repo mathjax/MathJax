51310e6df072832e370973a7bcc7a9a6befab36d
Davide P. Cervone <dpvc@union.edu>
2017-04-24 09:27:00 -0400

    Add copyright to latest.js

--- unpacked/latest.js
+++ unpacked/latest.js
@@ -1,3 +1,28 @@
+/*************************************************************
+ *
+ *  latest.js
+ *  
+ *  Replacement for cdn.mathjax.org/mathjax/latest that loads the
+ *  latest (2.x) version of MathJax from cdnjs, rawgit.com, or jsdelivr
+ *  depending on where it was loaded from.
+ *  
+ *  ---------------------------------------------------------------------
+ *  
+ *  Copyright (c) 2017 The MathJax Consortium
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 (function () {
 
    var CDN = {
