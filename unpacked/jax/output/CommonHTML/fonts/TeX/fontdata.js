3973b4db7b6a53f50e36b4a6b4b03dd4e7f6eb13
Davide P. Cervone <dpvc@union.edu>
2017-07-06 11:59:23 -0400

    Add MathJax_Vector font with non-combining vector to avoid problems with combining characters in recent WebKit versions.  Resolves issue #1709.

--- unpacked/jax/output/CommonHTML/fonts/TeX/fontdata.js
+++ unpacked/jax/output/CommonHTML/fonts/TeX/fontdata.js
@@ -95,6 +95,8 @@
                  remap: {0x391:0x41, 0x392:0x42, 0x395:0x45, 0x396:0x5A, 0x397:0x48,
                          0x399:0x49, 0x39A:0x4B, 0x39C:0x4D, 0x39D:0x4E, 0x39F:0x4F,
                          0x3A1:0x50, 0x3A4:0x54, 0x3A7:0x58,
+                         0xEEE2: [0xEEE2, "-TeX-vec"],    // HACK: for \vec (see #1709)
+                         0xE160: [0x2192, "-TeX-vec"],    // HACK: for \vec (see #1709)
                          0x2016:0x2225,
                          0x2216:[0x2216,"-TeX-variant"],  // \smallsetminus
                          0x210F:[0x210F,"-TeX-variant"],  // \hbar
@@ -154,6 +156,7 @@
                    0x2216:[0x2216,MML.VARIANT.NORMAL], // \setminus
                    0x210F:[0x210F,MML.VARIANT.NORMAL]  // \hslash
                  }},
+      "-TeX-vec": {fonts: ["MathJax_Vector"], cache:{}},  // HACK: non-combining \vec
       "-largeOp": {fonts:[SIZE2,SIZE1,MAIN,AMS],cache:{}},
       "-smallOp": {fonts:[SIZE1,MAIN,AMS], cache:{}},
       "-tex-caligraphic-bold": {fonts:["MathJax_Caligraphic-Bold","MathJax_Main-Bold"], bold:true, cache:{}, chain:"normal",
@@ -228,6 +231,8 @@
       0x2247: 0x2246,                 // wrong placement of this character
       0x231C: 0x250C, 0x231D:0x2510,  // wrong placement of \ulcorner, \urcorner
       0x231E: 0x2514, 0x231F:0x2518,  // wrong placement of \llcorner, \lrcorner
+
+      0x20D7: "\uEEE2\uE160",         // HACK: for non-combining \vec (#1709)
 
       //
       //  compound symbols not in these fonts
@@ -1590,6 +1595,12 @@
     0xE153: [333,0,450,-10,474],       // stix-horizontal brace, upper right piece
     0xE154: [120,0,400,-10,410]        // stix-oblique open face capital letter A
   };
+  
+  CHTML.FONTDATA.FONTS['MathJax_Vector'] = {
+    centerline: 357, ascent: 714, descent: 0,
+    0x2192: [714,-516,500.1,29,471],     // vector arrow (w = 500.1 to avoid zero width)
+    0xEEE2: [0,0,-500,0,0,{space:1}]   // backspace
+  };
 
   CHTML.FONTDATA.FONTS[MAIN][0x2212][0] = CHTML.FONTDATA.FONTS[MAIN][0x002B][0]; // minus is sized as plus
   CHTML.FONTDATA.FONTS[MAIN][0x2212][1] = CHTML.FONTDATA.FONTS[MAIN][0x002B][1]; // minus is sized as plus
@@ -1641,7 +1652,7 @@
   CHTML.FONTDATA.familyName = function (font) {
     font = font.replace(/^MathJax_/,"");
     var names = (font+"-Regular").split(/-/);
-    var suffix = names[0].toLowerCase().replace(/(?:igraphic|serif|writer|tur)$/,"") 
+    var suffix = names[0].toLowerCase().replace(/(?:igraphic|serif|writer|tur|tor)$/,"") 
                + "-" + names[1].replace(/[^A-Z]/g,"");
     return "MJXc-TeX-"+suffix;
   };
