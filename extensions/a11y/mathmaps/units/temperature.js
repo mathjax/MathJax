bfd5b1195f8c36c171a52f1a4b23a338042b6abe
Davide P. Cervone <dpvc@union.edu>
2017-07-21 16:16:33 -0400

    Add missing a11y SRE map files

---
+++ extensions/a11y/mathmaps/units/temperature.js
@@ -0,0 +1,31 @@
+[
+{"category": "",
+ "mappings": {
+   "default": {
+     "default": "Fahrenheit"
+     }
+   },
+ "key": "F",
+ "names": ["F", "F.", "\u00B0F"]
+},
+  {"category": "",
+ "mappings": {
+   "default": {
+     "default": "Celsius",
+     "alternative": "Centigrade"
+     }
+   },
+ "key": "C",
+ "names": ["C", "\u00B0C"]
+},
+  {"category": "",
+ "mappings": {
+   "default": {
+     "default": "Kelvin"
+     }
+   },
+ "key": "K",
+ "names": ["K", "\u00B0K"]
+}
+  
+]
