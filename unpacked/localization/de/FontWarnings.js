a6ac01928a06b008cbdc191c9b3fa8750c479b23
merloxx <kontakt@merlinchristen.de>
2018-12-16 00:47:30 +0100

    Fix typo in FontWarnings.js
    Fix typo in FontWarnings.js for german localization

--- unpacked/localization/de/FontWarnings.js
+++ unpacked/localization/de/FontWarnings.js
@@ -21,7 +21,7 @@ MathJax.Localization.addTranslation("de","FontWarnings",{
         version: "2.7.5",
         isLoaded: true,
         strings: {
-          webFont: "MathJax nutz web-basierte Fonts zur Darstellung der Mathematik auf dieser Seite. Da diese heruntergeladen werden m\u00FCssen, l\u00E4dt die Seite schneller, wenn Mathe-Fonts auf dem System installiert sind.",
+          webFont: "MathJax nutzt web-basierte Fonts zur Darstellung der Mathematik auf dieser Seite. Da diese heruntergeladen werden m\u00FCssen, l\u00E4dt die Seite schneller, wenn Mathe-Fonts auf dem System installiert sind.",
           imageFonts: "MathJax verwendet die Bildschriften anstatt der lokalen oder webbasierten Schriften. Das Rendern dauert l\u00E4nger als gew\u00F6hnlich und die Inhalte werden m\u00F6glicherweise nicht mit der vollen Aufl\u00F6sung deines Druckers ausgedruckt.",
           noFonts: "MathJax konnte keine Schriftart zur Anzeige der Inhalte finden und Bildschriften sind nicht verf\u00FCgbar. Es wird auf allgemeine Unicode-Zeichen zur\u00FCckgegriffen in der Hoffnung, dass dein Browser in der Lage ist, sie darzustellen. Einige Zeichen werden m\u00F6glicherweise nicht richtig oder gar nicht angezeigt.",
           webFonts: "Die meisten modernen Browser erlauben den Download von Schriften \u00FCber das Web. Eine Aktualisierung auf eine aktuellere Version deines Browsers (oder dessen Wechsel) kann die Qualit\u00E4t der Inhalte auf dieser Seite verbessern.",
