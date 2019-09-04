c926ed82b0201880d7c57e21c06db93766d32016
Davide P. Cervone <dpvc@union.edu>
2019-09-04 16:24:44 -0400

    Add the component files for v3.0.0

---
+++ es5/sre/mathmaps/fr/units/speed.js
@@ -0,0 +1,1 @@
+[{"locale":"fr"},{"key":"mph","mappings":{"default":{"default":"miles par heure","singular":"mile par heure","dual":""}},"names":["mph"]}]
---
+++ es5/sre/mathmaps/fr/units/temperature.js
@@ -0,0 +1,1 @@
+[{"locale":"fr"},{"key":"F","mappings":{"default":{"default":"farads","singular":"farad","dual":""}},"names":["F","F.","°F"]},{"key":"C","mappings":{"default":{"default":"coulombs","singular":"coulomb","dual":""}},"names":["C","°C"]},{"key":"K","mappings":{"default":{"default":"kelvins","singular":"kelvin","dual":""}},"names":["K","°K"]}]
