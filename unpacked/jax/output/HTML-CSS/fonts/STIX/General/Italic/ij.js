b2e4fbb57141c1b90117f4a7f57a9ac4b3da7359
Davide P. Cervone <dpvc@union.edu>
2020-02-22 14:40:11 -0500

    Add skew values for local STIX in HTML-CSS.  (mathjax/MathJax#2050)

--- unpacked/jax/output/HTML-CSS/fonts/STIX/General/Italic/ij.js
+++ unpacked/jax/output/HTML-CSS/fonts/STIX/General/Italic/ij.js
@@ -21,6 +21,10 @@
 MathJax.Hub.Insert(
   MathJax.OutputJax['HTML-CSS'].FONTDATA.FONTS['STIXGeneral-italic'],
   {
+    skew: {
+      0x1D6A4: .05,
+      0x1D6A5: .07,
+    },
     0x1D6A4: [441,11,278,47,235],      // MATHEMATICAL ITALIC SMALL DOTLESS I
     0x1D6A5: [441,207,278,-124,246]    // MATHEMATICAL ITALIC SMALL DOTLESS J
   }
